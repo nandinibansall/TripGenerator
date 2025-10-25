afe10590d040451ca3c272c552570fd0174727eb
nandinibansall <149954212+nandinibansall@users.noreply.github.com>
2025-10-25 15:36:13 +0530

    added package.json

--- backend/app/ConvexClientProvider.tsx
+++ backend/app/ConvexClientProvider.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import React, { ReactNode } from "react";
-import Provider from "./provider";
+import Provider from "../../Frontend/app/provider";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
