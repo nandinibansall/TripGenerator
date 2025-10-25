82fd1dfe7b7d369fb634c5dbb19b7d210ab73282
nandinibansall <149954212+nandinibansall@users.noreply.github.com>
2025-10-25 16:34:59 +0530

    corrected backend auth

--- backend/app/ConvexClientProvider.tsx
+++ backend/app/ConvexClientProvider.tsx
@@ -1,11 +1,15 @@
-"use client"
+"use client";
+
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import React, { ReactNode } from "react";
 import Provider from "../../Frontend/app/provider";
+
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>
-    <Provider>{children}</Provider>
-    </ConvexProvider>;
+  return (
+    <ConvexProvider client={convex}>
+      <Provider>{children}</Provider>
+    </ConvexProvider>
+  );
 }
